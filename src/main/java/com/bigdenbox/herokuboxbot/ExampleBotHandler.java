e25940eda43bd9bb12e53ffee5af6a81c2d5e618
bigdenbox <bigdenbox@gmail.com>
2022-03-14 16:58:04 +0200

    Commit bigdenbox
    String textSendMessage = "chatId = " + chatId + "; text = " + text;

--- src/main/java/com/bigdenbox/herokuboxbot/ExampleBotHandler.java
+++ src/main/java/com/bigdenbox/herokuboxbot/ExampleBotHandler.java
@@ -10,40 +10,42 @@ import org.telegram.telegrambots.meta.logging.BotLogger;
 
 public class ExampleBotHandler extends BotHandler {
 
-    @Override
-    public String getBotUsername() {
- //       return System.getenv("username");				// Heroku
-    	return "ExampleTelegramBoxBot";	// without heroku
-    }
+	@Override
+	public String getBotUsername() {
+		// return System.getenv("username"); // Heroku
+		return "ExampleTelegramBoxBot"; // without heroku
+	}
 
-    @Override
-    public String getBotToken() {
- //       return System.getenv("token");  // Heroku
-        return "5089646150:AAFgK7RlFlkhVASF9M_NiP2mhn9iRhr92JM"; // without heroku
-    }
+	@Override
+	public String getBotToken() {
+		// return System.getenv("token"); // Heroku
+		return "5089646150:AAFgK7RlFlkhVASF9M_NiP2mhn9iRhr92JM"; // without heroku
+	}
 
-    @Override
-    public BotApiMethod onUpdate(Update update) {
-        if (!update.hasMessage()) {
-            return null;
-        }
+	@Override
+	public BotApiMethod onUpdate(Update update) {
+		if (!update.hasMessage()) {
+			return null;
+		}
 
-        Message message = update.getMessage();
-        if (!message.hasText()) {
-            return null;
-        }
+		Message message = update.getMessage();
+		if (!message.hasText()) {
+			return null;
+		}
 
-        String text = message.getText();
-        long chatId = message.getChatId();
+		String text = message.getText();
+		long chatId = message.getChatId();
+//		String stringChatId = Long.toString(chatId);
+		String textSendMessage = "chatId = " + chatId + "; text = " + text;
 
-        SendMessage sm = new SendMessage(chatId, text);
-        SendMessage sm2 = new SendMessage(chatId, Long.toString(chatId));
-        try {
-            execute(sm);  // Send text echo
-            execute(sm2); // Send ChatId 
-        } catch (TelegramApiException e) {
-            BotLogger.error("SEND", e.toString());
-        }
-        return null;
-    }
+		SendMessage sm = new SendMessage(chatId, textSendMessage);
+		System.out.println(textSendMessage);
+
+		try {
+			execute(sm); 
+		} catch (TelegramApiException e) {
+			BotLogger.error("SEND", e.toString());
+		}
+		return null;
+	}
 }
